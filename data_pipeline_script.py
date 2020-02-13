106bcae6ffedceaaf5e834ba13f3307e6fbbcea2
Paul Metcalf <paulmetcalf@hotmail.com>
2020-02-13 10:30:41 +0000

    Added data retrieval to pipeline script.

--- data_pipeline_script.py
+++ data_pipeline_script.py
@@ -10,3 +10,9 @@ and then trains a deep neural network model to classify whether a wine is red or
 
 # Module Importations (A - Z)
 import pandas as pd
+
+# Retrieve data from ML repository
+
+white = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv", sep = ';')
+
+red = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv", sep = ';')
