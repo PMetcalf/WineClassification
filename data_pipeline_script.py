02b41b60f6cdbc309cfc55ea99080231578e166c
Paul Metcalf <paulmetcalf@hotmail.com>
2020-02-13 10:38:07 +0000

    Added data peek to pipeline.

--- data_pipeline_script.py
+++ data_pipeline_script.py
@@ -6,13 +6,17 @@ This script loads wine data from the UCI Machine Learning repository,
 performs data visualisation and preprocessing operations, 
 and then trains a deep neural network model to classify whether a wine is red or white.
 
+
 '''
 
 # Module Importations (A - Z)
 import pandas as pd
 
 # Retrieve data from ML repository
-
 white = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv", sep = ';')
 
 red = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv", sep = ';')
+
+# Peek at the retrieved data
+print(white.info())
+print(red.info())
