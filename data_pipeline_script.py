49413b5decf81352d06d4baa1598f9cb2e0add54
Paul Metcalf <paulmetcalf@hotmail.com>
2020-02-13 10:25:14 +0000

    Added pipeline script, workspace file and launch file to project.

---
+++ data_pipeline_script.py
@@ -0,0 +1,12 @@
+'''
+
+Data Pipeline Script
+
+This script loads wine data from the UCI Machine Learning repository, 
+performs data visualisation and preprocessing operations, 
+and then trains a deep neural network model to classify whether a wine is red or white.
+
+'''
+
+# Module Importations (A - Z)
+import pandas as pd
